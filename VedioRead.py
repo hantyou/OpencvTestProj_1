e0ea04b9628f86173776bf24e4810d28fe02188a
hantyou <daniel.zhai@yahoo.com>
2020-02-20 14:02:14 +0800

    加了一点注释

--- VedioRead.py
+++ VedioRead.py
@@ -5,36 +5,34 @@ import numpy as np
 
 # sysbg = cv2.createBackgroundSubtractorMOG2(500, 30, detectShadows=True)
 # sysKNN = cv2.createBackgroundSubtractorKNN(500, 50, detectShadows=True)
-filepath = r"D:\Programming\MATLAB\video_prog\FroggerHighway.mp4"
+"""文件的读取与视频文件的初始化"""
+filepath = r"D:\Programming\MATLAB\video_prog\VID_20200219_231622.mp4"
 vid = cv2.VideoCapture(filepath)
 flag = vid.isOpened()
 if flag:
     print("打开摄像头成功")
 else:
     print("打开摄像头失败")
-
 ret, frame = vid.read()
-size = (np.int(720*0.5), np.int(1080*0.5))
+size = (np.int(720 * 0.5), np.int(1080 * 0.5))
 frame = cv2.resize(frame, size, interpolation=cv2.INTER_CUBIC)
+"""过程中承载参数的矩阵的设置"""
 BG = np.zeros(shape=(frame.shape[0], frame.shape[1], 3), dtype=np.uint8)
 Sub = np.ones(shape=(frame.shape[0], frame.shape[1], 3), dtype=np.uint8)
 ColorSubShow = np.ones(shape=(frame.shape[0], frame.shape[1], 3), dtype=np.uint8)
 SubR = np.ones(shape=(frame.shape[0], frame.shape[1], 3), dtype=np.uint8)
 SubTemp = np.zeros(shape=(frame.shape[0], frame.shape[1], 3), dtype=np.uint8)
 ForeFlag = np.zeros(shape=(frame.shape[0], frame.shape[1], 3), dtype=np.uint8)
-"""更新率，a为背景更新率，b为前景更新率"""
-a = 0.05
+"""各类参数与过程中用到的计算用矩阵"""
+a = 0.05  # 更新率，a为背景更新率，b为前景更新率
 b = 0.0001
 BinaryThreshold = 30
-NumFrameForceForeToBack = 150 # 当一个目标多少帧之后，它会强制转换为背景
+NumFrameForceForeToBack = 100  # 当一个目标多少帧之后，它会强制转换为背景
 kernel1 = np.ones((5, 5), np.uint8)
 kernel2 = np.ones((11, 11), np.uint8)
 kernel3 = np.ones((3, 3), np.uint8)
 kernel4 = np.ones((9, 9), np.uint8)
 """显示窗口初始化"""
-# cv2.namedWindow("KNN Sub", cv2.WINDOW_KEEPRATIO)
-# cv2.namedWindow("Color Subtracted", cv2.WINDOW_KEEPRATIO)
-# cv2.namedWindow("Detect Mask", cv2.WINDOW_KEEPRATIO)
 cv2.namedWindow("Current Frame (Colored)", cv2.WINDOW_KEEPRATIO)
 cv2.namedWindow("Current Background (Colored)", cv2.WINDOW_KEEPRATIO)
 cv2.namedWindow("Sub (Colored)", cv2.WINDOW_KEEPRATIO)
@@ -64,6 +62,7 @@ UpdateWithinContours = True  # 以轮廓内物体为前景更新
 DoMorphology_1 = True  # 使用形态学处理消除小区域，先开后闭
 DoMorphology_2 = True  # 获得轮廓后使用形态学处理消除小区域，先开后闭
 GenContours = True  # 显示物体轮廓
+"""策略选择自动纠错环节"""
 if not GenContours:
     if UseMinimumRecContours:
         UseMinimumRecContours = False
@@ -78,10 +77,13 @@ else:
     else:
         if not UpdateWithinContours:
             print("虽然不规则轮廓被标出，但更新背景时并没有用到")
-
+"""视频目标追踪循环开始"""
 while 1:
     FrameNum += 1
     ret, frame = vid.read()
+    if not ret:
+        print("程序出错，没有正确读取视频，报错点在刚进入循环")
+        break
     frame = cv2.resize(frame, size, interpolation=cv2.INTER_CUBIC)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     """
@@ -116,14 +118,11 @@ while 1:
     BG = BG_Back + BG_Fore
     """
     Sub = cv2.absdiff(BG, frame)
-    # ColorDist = (Sub[:, :, 0] + Sub[:, :, 1]  + Sub[:, :, 2] )/3
-    # ret, SubShow = cv2.threshold(np.uint8(ColorDist),30, 255, type=cv2.THRESH_BINARY)
     ret, Sub = cv2.threshold(Sub, BinaryThreshold, 255, type=cv2.THRESH_BINARY)
     """是否做形态学处理"""
     if DoMorphology_1:
         Sub = cv2.morphologyEx(Sub, cv2.MORPH_OPEN, kernel1)  # 开
         Sub = cv2.morphologyEx(Sub, cv2.MORPH_CLOSE, kernel2)  # 闭
-
     """是否清除过长的前景"""
     if EliminateForegroundTooLong:
         ForeFlag = ForeFlag + np.uint8(Sub / 255)
@@ -149,19 +148,20 @@ while 1:
                 cv2.drawContours(FrameForContours, contours, i, (255, 255, 255), -1)
                 cv2.drawContours(SubTemp, contours, i, (255, 255, 255), -1)
         cv2.imshow("contours", FrameForContours)
+        """判断是否使用轮廓内的内容进行升级"""
         if UpdateWithinContours:
             Sub = np.where(SubTemp < 1, Sub, 255)
             if DoMorphology_2:
                 Sub = cv2.morphologyEx(Sub, cv2.MORPH_OPEN, kernel3)  # 开
                 Sub = cv2.morphologyEx(Sub, cv2.MORPH_CLOSE, kernel4)  # 闭
-    """判断是否使用轮廓内的内容进行升级"""
-
+    """如果不使用轮廓升级"""
     if not UpdateWithinContours:  # 不使用轮廓
+        """判断是否使用全部色彩层信息进行背景升级"""
         if updateAsAll:
             Sub[:, :, 0] = SubAll
             Sub[:, :, 1] = SubAll
             Sub[:, :, 2] = SubAll
-
+    """生成前景背景掩膜处理后的"""
     SubR = np.uint8(Sub / 255)  # SubR用来做掩模版，区分出前景和后景
     BG_ForeD = BG * SubR
     BG_BackD = BG - BG_ForeD
@@ -176,9 +176,6 @@ while 1:
     BG = BG_Back + BG_Fore
     cv2.imshow('Current Frame (Colored)', frame)
     cv2.imshow("Current Background (Colored)", BG)
-    # cv2.imshow("Color Subtracted", Show)
-    # cv2.imshow("Detect Mask", subtract)
-    # cv2.imshow("KNN Sub", subKNN)
     cv2.imshow("Sub (Colored)", ColorSubShow)
     cv2.imshow("Sub with All color channel merged", SubAll)
     if cv2.waitKey(1) & 0xFF == ord('q'):
